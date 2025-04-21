e9eec39451926ac1ada9a164d55dd6ef37dafa6a
Karthik-bn151193 <karthik.bn15111993@gmail.com>
2025-04-21 17:56:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-Terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  required_versions = "~> 1.11.4"
-}
 
 provider "aws" {
   region = var.aws_region
